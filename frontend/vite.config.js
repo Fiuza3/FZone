aa42ff09b9f1ecc065984911f7cd21f76768f91f
Marcus <marcusvinifiuza739@gmail.com>
2025-07-14 20:33:10 -0300

    chore: migrar de Vercel para Netlify
    - Remove arquivos de configuração do Vercel
    - Adiciona configuração do Netlify
    - Simplifica dependências e configuração do Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: false,
-        drop_debugger: true
-      }
-    }
-  },
-  optimizeDeps: {
-    exclude: ['fsevents']
-  }
+  plugins: [vue()]
 })
